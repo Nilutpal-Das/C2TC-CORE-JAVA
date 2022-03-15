51b0ddf81e8dff54b5b0c0487470ff4d411a09df
parth <parth@LAPTOP-HR1B4SJN>
2022-03-15 13:50:09 +0530

    String programs

---
+++ my project new/src/programs/String_Demo.java
@@ -0,0 +1,12 @@
+package programs;
+
+public class String_Demo {
+
+	public static void main(String[] args) {
+		StringBuffer s = new StringBuffer("Nilutpal");
+		s.append(" Das");
+		System.out.println(s);
+
+	}
+
+}
---
+++ my project new/src/programs/String_Demo2.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class String_Demo2 {
+
+	public static void main(String[] args) {
+		String s = "Nilutpal";
+		String upper = s.toUpperCase();
+		System.out.println(upper);
+		
+		
+		System.out.println(s.substring(3));
+		System.out.println(s.substring(1,5));
+				
+
+	}
+
+}
